e0d360e2c4efe6d70e7da2a3f9b0347c688fbf37
Caratacus <330952486@qq.com>
2017-06-17 01:25:51 +0800

    Add sequence and logic delete support

--- src/main/java/com/baomidou/mybatisplus/spring/boot/starter/GlobalConfig.java
+++ src/main/java/com/baomidou/mybatisplus/spring/boot/starter/GlobalConfig.java
@@ -16,6 +16,7 @@
 package com.baomidou.mybatisplus.spring.boot.starter;
 
 import com.baomidou.mybatisplus.entity.GlobalConfiguration;
+import com.baomidou.mybatisplus.incrementer.IKeyGenerator;
 import com.baomidou.mybatisplus.mapper.ISqlInjector;
 import com.baomidou.mybatisplus.mapper.MetaObjectHandler;
 import com.baomidou.mybatisplus.toolkit.StringUtils;
@@ -30,8 +31,6 @@ import com.baomidou.mybatisplus.toolkit.StringUtils;
  */
 public class GlobalConfig {
 
-    // 数据库类型
-    private String dbType;
     // 主键类型
     private Integer idType;
     // 表名、字段名、是否使用下划线命名
@@ -44,20 +43,16 @@ public class GlobalConfig {
     private Integer fieldStrategy;
     // 方便调试
     private Boolean refreshMapper;
-    // 是否自动获取DBType
-    private Boolean isAutoSetDbType;
     // 是否大写命名
     private Boolean isCapitalMode;
     // 标识符
     private String identifierQuote;
-
-    public String getDbType() {
-        return dbType;
-    }
-
-    public void setDbType(String dbType) {
-        this.dbType = dbType;
-    }
+    // 逻辑删除全局值
+    private String logicDeleteValue = null;
+    // 逻辑未删除全局值
+    private String logicNotDeleteValue = null;
+    // 表关键词 key 生成器
+    private String keyGenerator;
 
     public Integer getIdType() {
         return idType;
@@ -99,14 +94,6 @@ public class GlobalConfig {
         this.fieldStrategy = fieldStrategy;
     }
 
-    public Boolean getAutoSetDbType() {
-        return isAutoSetDbType;
-    }
-
-    public void setAutoSetDbType(Boolean autoSetDbType) {
-        isAutoSetDbType = autoSetDbType;
-    }
-
     public Boolean getCapitalMode() {
         return isCapitalMode;
     }
@@ -131,23 +118,50 @@ public class GlobalConfig {
         this.refreshMapper = refreshMapper;
     }
 
+    public String getLogicDeleteValue() {
+        return logicDeleteValue;
+    }
+
+    public void setLogicDeleteValue(String logicDeleteValue) {
+        this.logicDeleteValue = logicDeleteValue;
+    }
+
+    public String getLogicNotDeleteValue() {
+        return logicNotDeleteValue;
+    }
+
+    public void setLogicNotDeleteValue(String logicNotDeleteValue) {
+        this.logicNotDeleteValue = logicNotDeleteValue;
+    }
+
+    public String getKeyGenerator() {
+        return keyGenerator;
+    }
+
+    public void setKeyGenerator(String keyGenerator) {
+        this.keyGenerator = keyGenerator;
+    }
+
     public GlobalConfiguration convertGlobalConfiguration() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         GlobalConfiguration globalConfiguration = new GlobalConfiguration();
-        if (StringUtils.isNotEmpty(this.getDbType())) {
-            globalConfiguration.setDbType(this.getDbType());
-        }
         if (StringUtils.isNotEmpty(this.getIdentifierQuote())) {
             globalConfiguration.setIdentifierQuote(this.getIdentifierQuote());
         }
-
+        if (StringUtils.isNotEmpty(this.getLogicDeleteValue())) {
+            globalConfiguration.setLogicDeleteValue(this.getLogicDeleteValue());
+        }
+        if (StringUtils.isNotEmpty(this.getLogicNotDeleteValue())) {
+            globalConfiguration.setLogicNotDeleteValue(this.getLogicNotDeleteValue());
+        }
         if (StringUtils.isNotEmpty(this.getSqlInjector())) {
             globalConfiguration.setSqlInjector((ISqlInjector) Class.forName(this.getSqlInjector()).newInstance());
         }
-
         if (StringUtils.isNotEmpty(this.getMetaObjectHandler())) {
             globalConfiguration.setMetaObjectHandler((MetaObjectHandler) Class.forName(this.getMetaObjectHandler()).newInstance());
         }
-
+        if (StringUtils.isNotEmpty(this.getKeyGenerator())) {
+            globalConfiguration.setKeyGenerator((IKeyGenerator) Class.forName(this.getKeyGenerator()).newInstance());
+        }
         if (StringUtils.checkValNotNull(this.getIdType())) {
             globalConfiguration.setIdType(this.getIdType());
         }
@@ -160,13 +174,9 @@ public class GlobalConfig {
         if (StringUtils.checkValNotNull(this.getRefreshMapper())) {
             globalConfiguration.setRefresh(this.getRefreshMapper());
         }
-        if (StringUtils.checkValNotNull(this.getAutoSetDbType())) {
-            globalConfiguration.setAutoSetDbType(this.getAutoSetDbType());
-        }
         if (StringUtils.checkValNotNull(this.getCapitalMode())) {
             globalConfiguration.setCapitalMode(this.getCapitalMode());
         }
-
         return globalConfiguration;
     }
 
